4a10166b416e239de9397614a408e71006c84917
Thomas Cornell <tjcornell@wustl.edu>
2021-05-16 23:20:54 -0500

    removed unnecessary css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './components/styles/index.css';
 import App from './components/App';
 import 'bootstrap/dist/css/bootstrap.min.css'; //sourced from https://react-bootstrap.netlify.app/getting-started/introduction/#css
 
