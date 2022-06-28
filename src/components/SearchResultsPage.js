36a7b6794d8ff5e64054a8ff22587b709cf0d061
Thomas Cornell <tjcornell@wustl.edu>
2022-06-28 08:13:16 -0500

    removed annoying scrollTo

--- src/components/SearchResultsPage.js
+++ src/components/SearchResultsPage.js
@@ -25,8 +25,6 @@ export default function SearchResultsPage(props) {
             isShowingExpandedResult: false,
             expandedResultData: {}
         });
-
-        window.scrollTo(0, 0);
     }
 
     //returns a formatted date string based on the ISO Date provided by the API
@@ -54,7 +52,7 @@ export default function SearchResultsPage(props) {
 
     return (
         <>
-            <Container className='results-section m-1 mx-auto' fluid>
+            <Container fluid className='results-section mx-auto'>
                 {buildPageJsx()}
                 <div className='footer-text'>This product uses the <a className='tmdb-link' href='https://www.themoviedb.org/documentation/api' rel='noreferrer' target='_blank'>TMDb API</a> but is not endorsed or certified by TMDb.</div>
                 <Image className='attribution-logo pt-2' src='https://www.themoviedb.org/assets/2/v4/logos/v2/blue_short-8e7b30f73a4020692ccca9c88bafe5dcb6f8a62a4c6bc55cd9ba82bb2cd95f6c.svg' />
